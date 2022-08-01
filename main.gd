b8dc7dcf6dacce4f22d9733ed4b60cbd438c0b43
Marcelo02 <marcelo.manhaes.02@gmail.com>
2022-08-01 18:20:59 -0300

    documentação de código

--- main.gd
+++ main.gd
@@ -4,9 +4,9 @@ extends Node2D
 func _ready():
 	pass 
 
-
+# Função que identifica o clique do mouse
 func _on_Pedra_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		#print(event.as_text())
 		if event.is_action_pressed("mouse_left"):
-			print("click")
+			print("click") # Função de debug. APAGAR NA VERSÃO FINAL!
